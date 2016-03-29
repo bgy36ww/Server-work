8dd8e21f3dd68a30123151410f83512a3c4f8693
bgy36ww <david36ww@hotmail.com>
2016-03-28 23:29:20 -0400

    Update meetups-controller.js
    Make request on the front end

--- server/controllers/meetups-controller.js
+++ server/controllers/meetups-controller.js
@@ -19,13 +19,16 @@ module.exports.list = function (req, res) {
   res.json(req.body);
   Meetup.find({},function(err,result){});
 }
-
+//request a login interface on Angular JS that can use for testing this.
+//request a button and a test box to call this module for testing purpose
 module.exports.CreateUser = function (req, res) {
 
 }
+//request a button to call this module for testing purpose
 module.exports.SaveUser = function (req, res) {
 
 }
+//request a button and a text field that can display this output for testing purpose
 module.exports.Find = function (req, res) {
 
 }
